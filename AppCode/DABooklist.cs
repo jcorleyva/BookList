aa45c57b9d84b43d08a76a3262685e4bc5238f49
Joel Corley <JCorley@Gannett.com>
2015-06-25 12:18:46 -0400

    GDPI-684:  Synchronizing changes from production server to local host so that nothing gets missed.

---
+++ AppCode/DABooklist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CommonApp;
+using System.Configuration;
+using System.Data;
+
+namespace USATodayBookList.AppCode
+{
+    public class DABooklist : DACBase
+    {
+        public DABooklist()
+        {
+            base.ConnectionString = ConfigurationManager.ConnectionStrings["USATodayBookList"].ConnectionString;
+        }
+
+        public DataTable GetISBNWorkingList(int RecordCount, string ISBN, string Title, string Author, string Mode, string SortExpression)
+        {
+            return base.ExecuteStoredProcedure("GetISBNWorkingList", RecordCount, ISBN, Title, Author, Mode, SortExpression);
+        }
+
+        public void Archive()
+        {
+            base.ExecuteStoredProcedure("Archive");
+        }
+    }
+}
